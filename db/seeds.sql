19cbad48acfe1dc7ef2f58c2ede9301cf4d07b92
Robert Geyer <Robert Geyer>
2017-06-04 16:22:03 -0400

    First Commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name, eaten) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('California Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Carolina Burger', true);
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
